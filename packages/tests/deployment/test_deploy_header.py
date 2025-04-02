45e3f6e53faf449a909e0dc453ea3b1d85a60a66
Jiu-hong <jiu.hong.sun@gmail.com>
2025-04-02 18:25:34 +0800

    add deploy test

--- packages/tests/deployment/test_deploy_header.py
+++ packages/tests/deployment/test_deploy_header.py
@@ -8,3 +8,30 @@ from python_condor import DeployHeader
 account = "017e037b8b5621b9803cad20c2d85aca9b5028c5ee5238923bb4a8fc5131d539f5"
 chain_name = "integration-test"
 header = DeployHeader(account, chain_name)
+
+
+# === add_body_hash ===
+def test_header_add_body_hash():
+    assert not hasattr(header, "body_hash")
+    header.add_body_hash(
+        "7dca5f79b9d4c7e5fc0f15a34f22b9bb03ae1012071105ce499c2962c64d261d")
+    assert header.body_hash is not None
+
+# === to_bytes() ===
+
+
+def test_header_to_bytes():
+    result = header.to_bytes()
+    assert result == "xxx"
+
+
+# ==== byteHash()
+def test_header_byteHash():
+    result = header.byteHash()
+    assert result == "xxx"
+
+
+# ==== to_json()
+def test_header_to_json():
+    result = header.to_json()
+    assert result == "xxx"
---
+++ packages/tests/deployment/test_session_payment.py
@@ -0,0 +1,20 @@
+import pytest
+
+from python_condor import SessionPayment, CLU256, CLString
+
+
+# ===== SessionPayment =====
+
+runtime_args = {"arg1": CLU256(123), "arg2": CLString("hello")}
+session_payment = SessionPayment(123456)
+
+
+def test_session_payment_to_bytes():
+    result = session_payment.to_bytes().hex()
+    assert result == "00000000000100000006000000616d6f756e74040000000340e20108"
+
+
+def test_session_payment_to_json():
+    result = session_payment.to_json()
+    assert result == {'payment': {'ModuleBytes': {'module_bytes': '', 'args': [
+        ('amount', {'cl_type': 'U512', 'bytes': '0340e201', 'parsed': 123456})]}}}

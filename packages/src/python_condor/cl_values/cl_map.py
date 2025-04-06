f72ee8518d0dc3a4e23bdadf9add07c573650ff3
Jiu-hong <jiu.hong.sun@gmail.com>
2025-04-06 09:26:33 +0800

    remove useless

--- packages/src/python_condor/cl_values/cl_map.py
+++ packages/src/python_condor/cl_values/cl_map.py
@@ -15,14 +15,3 @@ class CLMap(CLValue):
 
     def serialize(self):
         return self.__get_list__().sorted().serialize()
-
-
-# a = CLMap({CLU8(3): CLString("Jim"), CLU8(
-#     2): CLString("Jack"), CLU8(4): CLString("Jane"), CLU8(1): CLString("Jill")})
-# print(a.serialize())
-# print("str=>", a.to_json())
-# print("value=>", a.value())
-# print("type=>", a.cl_type())
-# print("a", a.to_json())
-# print("clvalue:", a.cl_value())
-# print("value:", a.value())
